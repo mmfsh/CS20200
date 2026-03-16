1adef5b8f5192b215531afeb87abede9211d2a49
mmfsh <psh531531@kaist.ac.kr>
2026-03-16 09:38:44 +0900

    Act03

--- Activities/Act03/Program.fs
+++ Activities/Act03/Program.fs
@@ -2,8 +2,10 @@
 
 /// Modify the function `gcd` in such a way that the function computes the
 /// greatest common divisor of two 32-bit integers.
-let gcd a b =
-  0
+let rec gcd a b =
+  if b = 0 then a
+  elif a = 0 then b
+  else gcd b (a % b)
 
 gcd 360 210
 |> printfn "%d"
